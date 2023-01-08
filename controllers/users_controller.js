584278025f98439332fae87726152543db0d1c48
prashant <prashagupt@gmail.com>
2023-01-09 00:06:54 +0530

    passport-js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -10,6 +10,10 @@ module.exports.profile = function(req,res){
 
 //render the sign up page
 module.exports.signUp = function(req,res){
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+    
     return res.render('user_sign_up',{
         title: "Codeial sign up"
     })
@@ -17,6 +21,11 @@ module.exports.signUp = function(req,res){
 
 //render the sign in page
 module.exports.signIn = function(req,res){
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
+
     return res.render('user_sign_in',{
         title: "Codeial sign in"
     })
@@ -46,6 +55,11 @@ module.exports.create = function(req,res){
 }
 
 module.exports.createSession = function(req,res){
-    // to do later
+    return res.redirect('/');
+}
 
+module.exports.destroySession = function(req,res){
+    req.logout(function(err) {
+        if (err) { return next(err);}});
+    return res.redirect('/');
 }
--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const expressLayouts=require('express-ejs-layouts');
 
 const db = require('./config/mongoose');
 
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo');
+
 app.use(express.urlencoded());
 
 app.use(cookieParser());
@@ -22,12 +27,40 @@ app.set('layout extractScripts',true);
 
 // use express router
 
-app.use('/',require('./routes'));
 
 // set up view engine
 app.set('view engine','ejs');
 app.set('views','./views');
 
+app.use(session({
+    name: 'codeial',
+    // to do change the secret before deployment
+    secret: 'wkvkwbv',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store: MongoStore.create(
+        {
+
+            mongoUrl:'mongodb://0.0.0.0/codeial_devlopment',
+            autoRemove: 'disabled'
+        },
+        function(err){
+            console.log(err || 'connect-mongodb steup ok');
+        }
+
+    )
+
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+app.use('/',require('./routes'));
 
 app.listen(port,function(err){
     if(err){
