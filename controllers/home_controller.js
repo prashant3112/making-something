fe20a20db2231c667561ac3ad4732c5bca2eb537
prashant <prashagupt@gmail.com>
2023-01-10 19:46:47 +0530

    comment added post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,27 @@
+const Post = require('../models/post'); 
+
 module.exports.home= function(req,res){
     //return res.end('<h1>Express is up for codeial!</h1>')
-    return res.render('home',{
+    /*return res.render('home',{
         title: "Home"
+    });*/
+    Post.find({}, function(err, posts){
+        return res.render('home',{
+            title: "Codeial | Home",
+            posts: posts
+        });
     });
+
+    /*Post.find({}).populate('user').populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    }).exec(function(err, posts){
+        return res.render('home',{
+            title: "Codeial | Home",
+            posts: posts
+        });
+    })*/
+
 }
